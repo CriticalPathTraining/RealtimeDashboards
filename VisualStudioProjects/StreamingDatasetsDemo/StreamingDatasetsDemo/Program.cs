0b2503b24f13679514356c2e6baf05286f640d02
Ted Pattison <ted@tedpattison.net>
2017-10-06 11:14:10 -0400

    Updates

---
+++ VisualStudioProjects/StreamingDatasetsDemo/StreamingDatasetsDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StreamingDatasetsDemo {
+  class Program {
+
+    static PowerBiServiceWrapper pbiService = new PowerBiServiceWrapper();
+
+    static void Main() {
+      pbiService.CreateDemoStreamingDataset("Demo 1: Streaming Dataset");
+     // pbiService.CreateDemoHybridDataset("Demo 2: Hybrid Dataset");
+     // pbiService.CreateDemoPushDataset("Demo 3: Push Dataset");
+
+    }
+  }
+
+}
